71f557189e2d24dc8bc925bd877cd18b39cf288d
Habibur Rahman Nayan <habiburrahmannayan66@gmail.com>
2023-10-04 23:19:19 +0600

    banner and category added

--- src/Pages/Shared/Navber/Navber.jsx
+++ src/Pages/Shared/Navber/Navber.jsx
@@ -25,7 +25,7 @@ const Navber = () => {
     </>
   );
   return (
-    <div className="navbar bg-base-100">
+    <div className="navbar fixed z-10 bg-opacity-30 max-w-screen-xl  bg-black text-info">
       <div className="navbar-start">
         <div className="dropdown">
           <label tabIndex={0} className="btn btn-ghost lg:hidden">
